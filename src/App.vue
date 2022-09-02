c4586029ade2cfc9af0b09fbba88cbe0c27c2380
Artem Isaakyan <artem.issakyan@raketa.agency>
2022-09-02 17:06:54 +0300

    first

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <Documentation />
+  </div>
+</template>
+
+<script>
+import Documentation from "./components/Documentation.vue";
+  export default {
+    components: { Documentation }
+  }
+</script>
+
+<style>
+  .wrapper {
+    font-family: 'Roboto Condensed';
+    max-width: 1240px;
+    width: 100%;
+    margin: 0 auto;
+  }
+</style>
---
+++ src/components/Documentation.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="documentation">
+        
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+
+</style>
